444b1cde24e836c1a4cc03ea9790c0316acb49c5
Gowtham Shankar <gowthamshankar@sgowthamsha-a01.vmware.com>
2022-06-01 12:44:48 -0400

    first commit

---
+++ src/test/java/com/test/loginservicenew/LoginServiceNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.loginservicenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginServiceNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
