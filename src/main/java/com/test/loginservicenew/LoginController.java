1dc1ac7af28ec0e68f74372602e67e4dec9e59b0
Gowtham Shankar <gowthamshankar@sgowthamsha-a01.vmware.com>
2022-06-02 22:03:41 -0400

    Add service

--- src/main/java/com/test/loginservicenew/LoginController.java
+++ src/main/java/com/test/loginservicenew/LoginController.java
@@ -1,14 +1,21 @@
 package com.test.loginservicenew;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class LoginController {
+
+    @Autowired
+    private RestTemplate restTemplate;
     
     @RequestMapping(value = "/fetchCardDetails", method = RequestMethod.GET)
     public String hello() {
-        return "hello world!";
+
+        String response = restTemplate.getForObject("http://order-service/order/order/user", String.class);
+        return response + " - data from order service";
     }
 }
